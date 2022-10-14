0adc8829ed44a1d54d205924142247360c6dfc76
Gu3r0 <jaimerodriguez123456@gmail.com>
2022-10-14 02:23:36 -0400

    Version 0.10.10

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,26 +1,27 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import { AuthRoutes } from "../auth/routes/AuthRoutes";
-import { useCheckAuth } from "../hooks";
+import {useCheckAuth} from "../hooks";
 import { JournalRoutes } from "../journal/routes/JournalRoutes";
 import { CheckingAuth } from "../ui";
 
 export const AppRouter = () => {
-  const status = useCheckAuth();
+  
+  const status  = useCheckAuth();
 
   if (status === "checking") {
     return <CheckingAuth />;
   }
 
   return (
-    <>
-      <Routes>
-        {status === "authenticated" ? (
-          <Route path="/*" element={<JournalRoutes />} />
-        ) : (
-          <Route path="/auth/*" element={<AuthRoutes />} />
-        )}
-        <Route path="/*" element={<Navigate to="/auth/login" />} />
-      </Routes>
-    </>
+  <>
+    <Routes>
+      {status === "authenticated" ? (
+        <Route path="/*" element={<JournalRoutes />} />
+      ) : (
+        <Route path="/auth/*" element={<AuthRoutes />} />
+      )}
+      <Route path="/*" element={<Navigate to="/auth/login" />} />
+    </Routes>
+  </>
   );
 };
